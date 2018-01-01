ce427992fdeaf69c1a357a507f9caf5558590ef8
David M <david.magee15@yahoo.com>
2018-01-01 14:53:54 +0000

    added smooth scrolling, added link buttons to view projects

--- public/css/script.js
+++ public/css/script.js
@@ -1,10 +1,24 @@
 $(window).on("scroll", function() {
     if($(window).scrollTop() > $(window).height()) {
+        $(".header").addClass("headerActive");
         $(".headerButton").addClass("active");
         $(".buttonList").addClass("activeBorder");
     } else {
         //remove the background property so it comes transparent again (defined in your css)
+       $(".header").removeClass("headerActive");
        $(".headerButton").removeClass("active");
        $(".buttonList").removeClass("activeBorder");
     }
+});
+
+$("#workButton").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".workSection").offset().top},
+        'slow');
+});
+
+$("#aboutButton").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".coverSection").offset().top},
+        'slow');
 });
