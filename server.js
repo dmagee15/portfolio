aae22b646274791b7487628054e9a3fc6ad4a21e
David M <david.magee15@yahoo.com>
2018-04-17 01:26:48 +0000

    removed mongo, passport, and other superfluous code

--- server.js
+++ server.js
@@ -2,20 +2,12 @@
 
 var express = require('express');
 var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
 var session = require('express-session');
 var bodyparser = require('body-parser');
-var flash = require('connect-flash');
 var path = require('path');
-var googleBooks = require('google-books-search');
 
 var app = express();
 require('dotenv').load();
-require('./app/config/passport')(passport);
-
-mongoose.connect(process.env.MONGO_URI);
-mongoose.Promise = global.Promise;
 
 app.set('view engine', 'pug');
 app.set("views", path.join(__dirname, "views"));
@@ -34,11 +26,7 @@ app.use(session({
 	saveUninitialized: true
 }));
 
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-
-routes(app, passport, googleBooks);
+routes(app);
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
