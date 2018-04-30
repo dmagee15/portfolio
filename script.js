9a79686207b84d06482e05cbd8ab3e2247381eee
dmagee15 <david.magee15@yahoo.com>
2018-04-30 16:45:20 -0600

    improved new format for work entries, added all entries

--- script.js
+++ script.js
@@ -31,32 +31,48 @@ $("#contactButton").click(function() {
 
 $("#workEntryInfo1").on("click", function(){
     if($("#workEntryInfo1Box").hasClass('hide')) {
-        $("#workEntryInfo1Box").animate({maxHeight:'1000px'},300).removeClass('hide');
+        $("#workEntryInfo1Box").animate({maxHeight:'1000px', paddingBottom:'15px'},300).removeClass('hide');
       } else { 
-        $("#workEntryInfo1Box").animate({maxHeight:0},300).addClass('hide');
+        $("#workEntryInfo1Box").animate({maxHeight:0, paddingBottom:0},300).addClass('hide');
       }
 });
 
 $("#workEntryInfo2").on("click", function(){
     if($("#workEntryInfo2Box").hasClass('hide')) {
-        $("#workEntryInfo2Box").animate({maxHeight:'1000px'},300).removeClass('hide');
+        $("#workEntryInfo2Box").animate({maxHeight:'1000px', paddingBottom:'15px'},300).removeClass('hide');
       } else { 
-        $("#workEntryInfo2Box").animate({maxHeight:0},300).addClass('hide');
+        $("#workEntryInfo2Box").animate({maxHeight:0, paddingBottom:0},300).addClass('hide');
       }
 });
 
 $("#workEntryInfo3").on("click", function(){
     if($("#workEntryInfo3Box").hasClass('hide')) {
-        $("#workEntryInfo3Box").animate({maxHeight:'1000px'},300).removeClass('hide');
+        $("#workEntryInfo3Box").animate({maxHeight:'1000px', paddingBottom:'15px'},300).removeClass('hide');
       } else { 
-        $("#workEntryInfo3Box").animate({maxHeight:0},300).addClass('hide');
+        $("#workEntryInfo3Box").animate({maxHeight:0, paddingBottom:0},300).addClass('hide');
       }
 });
 
 $("#workEntryInfo4").on("click", function(){
     if($("#workEntryInfo4Box").hasClass('hide')) {
-        $("#workEntryInfo4Box").animate({maxHeight:'1000px'},300).removeClass('hide');
+        $("#workEntryInfo4Box").animate({maxHeight:'1000px', paddingBottom:'15px'},300).removeClass('hide');
       } else { 
-        $("#workEntryInfo4Box").animate({maxHeight:0},300).addClass('hide');
+        $("#workEntryInfo4Box").animate({maxHeight:0, paddingBottom:0},300).addClass('hide');
+      }
+});
+
+$("#workEntryInfo5").on("click", function(){
+    if($("#workEntryInfo5Box").hasClass('hide')) {
+        $("#workEntryInfo5Box").animate({maxHeight:'1000px', paddingBottom:'15px'},300).removeClass('hide');
+      } else { 
+        $("#workEntryInfo5Box").animate({maxHeight:0, paddingBottom:0},300).addClass('hide');
+      }
+});
+
+$("#workEntryInfo6").on("click", function(){
+    if($("#workEntryInfo6Box").hasClass('hide')) {
+        $("#workEntryInfo6Box").animate({maxHeight:'1000px', paddingBottom:'15px'},300).removeClass('hide');
+      } else { 
+        $("#workEntryInfo6Box").animate({maxHeight:0, paddingBottom:0},300).addClass('hide');
       }
 });
